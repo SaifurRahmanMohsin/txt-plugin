edde173f24921015df47ddc66be2e2f718a07f75
Saifur Rahman Mohsin <timetraveller1992@gmail.com>
2015-03-12 01:35:58 +0530

    favicon
    Prefer favicon in root of website than using meta tag

--- routes.php
+++ routes.php
@@ -20,19 +20,27 @@ Route::get('humans.txt', function (){
 Route::get('browserconfig.xml', function (){
   if (!$theme = Theme::getEditTheme())
     throw new ApplicationException('Unable to find the active theme.');
-  $app_path = URL::to('/'). '/themes/'. $theme->getDirName() . '/assets/images/ico';
+  $ico_path = URL::to('/'). '/themes/' . $theme->getDirName() . '/assets/images/ico';
   $msft_icons = <<< THEXML
 <?xml version="1.0" encoding="utf-8"?>
 <browserconfig>
     <msapplication>
         <tile>
-            <square70x70logo src="$app_path/tile.png"/>
-            <square150x150logo src="$app_path/tile.png"/>
-            <wide310x150logo src="$app_path/tile-wide.png"/>
-            <square310x310logo src="$app_path/tile.png"/>
+            <square70x70logo src="$ico_path/tile.png"/>
+            <square150x150logo src="$ico_path/tile.png"/>
+            <wide310x150logo src="$ico_path/tile-wide.png"/>
+            <square310x310logo src="$ico_path/tile.png"/>
         </tile>
     </msapplication>
 </browserconfig>
 THEXML;
     return Response::make($msft_icons, 200, array('Content-Type' => 'application/xml'));
+});
+
+Route::get('favicon.ico', function (){
+  if (!$theme = Theme::getEditTheme())
+    throw new ApplicationException('Unable to find the active theme.');
+  $ico_path = themes_path() . '/' . $theme->getDirName() . '/assets/images/ico/favicon.ico';
+  $image = File::get($ico_path);
+    return Response::make($image, 200, ['content-type' => 'image/x-icon']);
 });
