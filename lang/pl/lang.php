d9a2df34058ea8746c3f02ab7e5afc078dcfc0fb
Piotr Kubisa <ovirendo@gmail.com>
2015-06-06 16:12:29 +0200

    Translated to Polish lang.

---
+++ lang/pl/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+  'plugin' => [
+    'name' => 'Txt',
+    'description' => 'Pozwala na zarządzanie rekordami w humans.txt i robots.txt.'
+  ],
+  'robots' => [
+    'label' => 'Robots',
+    'description' => 'Dodaj rekordy do robots.txt.'
+  ],
+  'humans' => [
+    'label' => 'Humans',
+    'description' => 'Dodaj rekordy do human.txt.'
+  ],
+  'agents' => [
+    'label' => 'User-Agents',
+    'description' => 'Zarządzaj agentami (user-agents).'
+  ],
+  'settings' => [
+    'label' => 'Ustawienia',
+    'description' => 'Zarządzaj konfiguracją modułu Txt.',
+  ],
+  'exception' => [
+    'ask_enable' => 'Najpierw włącz możliwość edycji :file w ustawieniach by zobaczyć tą stronę',
+  ]
+];
